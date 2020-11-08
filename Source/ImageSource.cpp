77538a5c61f15ae73af89d79f3bdc354f7dd4ed4
v0lt <mpc-be@mail.ru>
2020-11-08 08:03:16 +0300

    Используем макрос ZeroMemory.

--- Source/ImageSource.cpp
+++ Source/ImageSource.cpp
@@ -403,7 +403,7 @@ CImageStream::CImageStream(const WCHAR* name, CSource* pParent, HRESULT* phr)
 		mt.SetSampleSize(bufferSize1);
 
 		VIDEOINFOHEADER2* vih2 = (VIDEOINFOHEADER2*)mt.AllocFormatBuffer(sizeof(VIDEOINFOHEADER2));
-		memset(vih2, 0, sizeof(VIDEOINFOHEADER2));
+		ZeroMemory(vih2, sizeof(VIDEOINFOHEADER2));
 		vih2->rcSource                = { 0, 0, (long)m_Width, (long)m_Height};
 		vih2->rcTarget                = vih2->rcSource;
 		vih2->AvgTimePerFrame         = m_AvgTimePerFrame;
