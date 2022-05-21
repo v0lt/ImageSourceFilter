721fbe259ae7c57ad4e7cb8dc186eb25528b6051
v0lt <mpc-be@mail.ru>
2022-05-21 21:33:25 +0300

    Косметическая переделка функции DllRegisterServer.

--- Source/dllmain.cpp
+++ Source/dllmain.cpp
@@ -51,35 +51,37 @@ int g_cTemplates = std::size(g_Templates);
 
 STDAPI DllRegisterServer()
 {
-	const struct {
-		LPCWSTR name;
-		LPCWSTR data;
-	} values[] {
-		{ L"0",  L"0,8,,89504E470D0A1A0A" },          // PNG
-		{ L"1",  L"0,3,,FFD8FF" },                    // JPEG
-		{ L"2",  L"0,2,,424D,6,4,,00000000" },        // BMP
-		{ L"3",  L"0,4,,49492A00" },                  // TIFF, DNG and other
-		{ L"4",  L"0,4,,4949BC01" },                  // HD Photo/JPEG XR
-		{ L"5",  L"0,4,,52494646,8,4,,57454250" },    // WebP
-		{ L"6",  L"4,8,,667479706D696631,16,8,,6D69663168656963" }, // HEIF (.heic)
-		{ L"7",  L"4,8,,6674797068656963,16,8,,6D69663168656963" }, // HEIF (.heif)
-		{ L"8",  L"4,8,,6674797061766966,16,8,,6D69663161766966" }, // AVIF (.avif)
-		{ L"9",  L"0,4,,44445320" },                  // DDS
-		{ L"10", L"0,4,,4949524F" },                  // ORF
-		{ L"11", L"0,4,,4D4D002A" },                  // PEF
-		{ L"12", L"0,2,,FF0A" },                      // JPEG XL codestream
-		{ L"13", L"0,12,,0000000C4A584C200D0A870A" }, // JPEG XL container
-		// This may not work for JPEG and BMP. "Generate Still Video" still connects.
-		{ L"Source Filter", _CRT_WIDE(STR_CLSID_ImageSource) },
+	const LPCWSTR value_data[] {
+		_CRT_WIDE(STR_CLSID_ImageSource),
+		L"0,8,,89504E470D0A1A0A",          // PNG
+		L"0,3,,FFD8FF",                    // JPEG
+		L"0,2,,424D,6,4,,00000000",        // BMP
+		L"0,4,,49492A00",                  // TIFF, DNG and other
+		L"0,4,,4949BC01",                  // HD Photo/JPEG XR
+		L"0,4,,52494646,8,4,,57454250",    // WebP
+		L"4,8,,667479706D696631,16,8,,6D69663168656963", // HEIF (.heic)
+		L"4,8,,6674797068656963,16,8,,6D69663168656963", // HEIF (.heif)
+		L"4,8,,6674797061766966,16,8,,6D69663161766966", // AVIF (.avif)
+		L"0,4,,44445320",                  // DDS
+		L"0,4,,4949524F",                  // ORF
+		L"0,4,,4D4D002A",                  // PEF
+		L"0,2,,FF0A",                      // JPEG XL codestream
+		L"0,12,,0000000C4A584C200D0A870A", // JPEG XL container
 	};
 
 	HKEY hKey;
 	LONG ec = ::RegCreateKeyExW(HKEY_CLASSES_ROOT, L"Media Type\\{e436eb83-524f-11ce-9f53-0020af0ba770}\\" _CRT_WIDE(STR_CLSID_ImageSource), 0, 0, REG_OPTION_NON_VOLATILE, KEY_ALL_ACCESS, 0, &hKey, 0);
 	if (ec == ERROR_SUCCESS) {
-		for (const auto& value : values) {
-			ec = ::RegSetValueExW(hKey, value.name, 0, REG_SZ,
-				reinterpret_cast<BYTE*>(const_cast<LPWSTR>(value.data)),
-				(DWORD)(wcslen(value.data) + 1) * sizeof(WCHAR));
+		ec = ::RegSetValueExW(hKey, L"Source Filter", 0, REG_SZ,
+			reinterpret_cast<BYTE*>(const_cast<LPWSTR>(value_data[0])),
+			(DWORD)(wcslen(value_data[0]) + 1) * sizeof(WCHAR));
+
+		WCHAR value_name[4] = {};
+		for (int i = 1; i < std::size(value_data); i++) {
+			_itow_s(i, value_name, 10);
+			ec = ::RegSetValueExW(hKey, value_name, 0, REG_SZ,
+				reinterpret_cast<BYTE*>(const_cast<LPWSTR>(value_data[i])),
+				(DWORD)(wcslen(value_data[i]) + 1) * sizeof(WCHAR));
 		}
 
 		::RegCloseKey(hKey);
