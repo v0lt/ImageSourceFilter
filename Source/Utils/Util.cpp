ffaf79890797adb67b7cc90383fab1b1673d6f94
v0lt <mpc-be@mail.ru>
2022-09-29 17:52:12 +0300

    Добавлена расшифровка ошибки WINCODEC_ERR_PROPERTYUNEXPECTEDTYPE.

--- Source/Utils/Util.cpp
+++ Source/Utils/Util.cpp
@@ -65,6 +65,7 @@ std::wstring HR2Str(const HRESULT hr)
 		// some COM Error Codes (UI, Audio, DirectX, Codec) https://docs.microsoft.com/en-us/windows/win32/com/com-error-codes-10
 		UNPACK_VALUE(WINCODEC_ERR_COMPONENTNOTFOUND);
 		UNPACK_VALUE(WINCODEC_ERR_UNSUPPORTEDPIXELFORMAT);
+		UNPACK_VALUE(WINCODEC_ERR_PROPERTYUNEXPECTEDTYPE);
 		// some System Error Codes
 		UNPACK_HR_WIN32(ERROR_MOD_NOT_FOUND);
 		UNPACK_HR_WIN32(ERROR_INVALID_WINDOW_HANDLE);
